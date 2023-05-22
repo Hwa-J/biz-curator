579516ebd2a9f4435cfe9aa1534c4c1537f7fd58
Hwa-J <hj1103.code@gmail.com>
2023-05-22 22:06:45 +0900

    타입 수정

--- utils/types/responseType.ts
+++ utils/types/responseType.ts
@@ -1,9 +1,10 @@
 export interface NoticePostType {
-    id: number;
-    title: string;
-    content: string;
-    date: string;
-    isFixed: boolean;
-    previousId?: number;
-    nextId?: number;
+  [x: string]: any;
+  id: number;
+  title: string;
+  content: string;
+  date: string;
+  isFixed: boolean;
+  previousId?: number;
+  nextId?: number;
 }
