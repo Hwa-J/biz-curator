4682092714db3559f81019ea53f8f9a68a203d27
Hwa-J <hj1103.code@gmail.com>
2023-05-25 00:02:28 +0900

    s3 배포

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: 'imgix',
-    path: 'https://s3-ebnatural.s3.ap-northeast-2.amazonaws.com',
     remotePatterns: [
       {
         protocol: 'https',
@@ -14,7 +12,6 @@ const nextConfig = {
     ],
   },
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-  output: 'export',
 };
 
 module.exports = nextConfig;
