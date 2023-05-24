f9f60ba68b5c67be7dec6c0da40867e57f955afe
Hwa-J <hj1103.code@gmail.com>
2023-05-25 00:10:54 +0900

    Mixed content 문제 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js" async />
       <body>
         <Main />
